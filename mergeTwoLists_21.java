d921799d9e5277ea8e7a61beef038e2655e95b94
xminao <xminao@yeah.net>
2023-04-04 11:21:13 +0800

    Leetcode (21): Merge Two Sorted Lists.

---
+++ mergeTwoLists_21.java
@@ -0,0 +1,40 @@
+public class mergeTwoLists_21 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    /**
+     * 21. 合并两个有序链表
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     *
+     * 解法：
+     *      类似归并排序。
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                p.next = list1;
+                list1 = list1.next;
+            } else {
+                p.next = list2;
+                list2 = list2.next;
+            }
+            p = p.next;
+        }
+
+        if (list1 == null) {
+            p.next = list2;
+        } else if (list2 == null) {
+            p.next = list1;
+        }
+
+        return dummy.next;
+    }
+
+}
