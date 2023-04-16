a942e48229671da9eda00f40f4efcb0a13ccd1cb
xminao <xminao@yeah.net>
2023-04-16 20:54:27 +0800

    Leetcode (88): Merge Sorted Array.

---
+++ merge_88.java
@@ -0,0 +1,24 @@
+public class merge_88 {
+    /**
+     * 88. 合并两个有序数组
+     * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+     *
+     * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+     *
+     * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+     *
+     * 解法：
+     *      从后向前排。双指针。
+     */
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = nums1.length;
+        while (n > 0) {
+            if (m > 0 && nums1[m - 1] > nums2[n - 1]) {
+                nums1[--i] = nums1[--m];
+            } else { // m排完了或者nums2的末尾大于nums1的末尾。
+                nums1[--i] = nums2[--n];
+            }
+        }
+    }
+}
