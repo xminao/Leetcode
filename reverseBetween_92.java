af8ba0fccfcd0e7778593e874d8a04148fe92eb6
xminao <xminao@yeah.net>
2023-05-10 23:32:26 +0800

    Leetcode (92): Reverse Linked List II.

---
+++ reverseBetween_92.java
@@ -0,0 +1,34 @@
+/**
+ * 92. 反转链表 II
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+
+ */
+public class reverseBetween_92 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+    private ListNode successor = null;
+
+    private ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+}
